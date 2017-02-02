177c8a88cfc74a96c69337d171b85ce03891c92b
jay <jr30149_@hotmail.com>
2017-02-02 14:59:59 +0700

    dfgdfg

--- index.php
+++ index.php
@@ -18,8 +18,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'asdasdasd',
-				'text' => $text
+				'type' => 'text',
+				'text' => "asddasdasd"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
