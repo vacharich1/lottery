f26110ab4e53e79a721fb4f4e82794de5e17342b
jay <jr30149_@hotmail.com>
2017-12-21 12:15:26 +0700

    dfgdgdf

--- index.php
+++ index.php
@@ -185,12 +185,12 @@ if (!is_null($events['events'])) {
 							];	
 							//$date = new DateTime('now');
 							//$dtz = new DateTimeZone("Asia/Bangkok"); //Your timezone
-							//$dateuse=CURDATE();
+							//$dateuse=NOW(), INTERVAL 7 HOUR
 							
 
 							
 							$sql = "INSERT INTO numberfromuser(id, userid, telephone, number, date)
-										VALUES ('', '$userid', '$telephone', '$text', NOW())";
+										VALUES ('', '$userid', '$telephone', '$text', (NOW(), INTERVAL 7 HOUR))";
 													
 										if (mysqli_query($link, $sql)) {
 													echo "New record created successfully";
